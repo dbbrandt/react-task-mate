a1e98c4a3d0a7faedc3696bd981f5bc472a8d687
Daniel Brandt <dbbrandt@gmail.com>
2024-01-10 12:22:07 -0500

    Add a status updated check box that calls the updateTask mutation.

--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from "react";
-import {Task, useDeleteTaskMutation} from "../../generated/graphql-frontend";
+import {Task, TaskStatus, useDeleteTaskMutation, useUpdateTaskMutation} from "../../generated/graphql-frontend";
 import Link from "next/link";
 
 interface TaskItemProps {
@@ -42,8 +42,39 @@ const TaskListItem: React.FC<TaskItemProps> = ({task, onSuccess}) => {
 
    },[error])
 
+    const [
+        updateTask,
+        { loading: updateTaskLoading, error: updateTaskError },
+    ] = useUpdateTaskMutation({
+        errorPolicy: 'all',
+        onCompleted: () => {
+            onSuccess();
+        },
+    });
+    const handleStatusChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newStatus = e.target.checked
+            ? TaskStatus.Completed
+            : TaskStatus.Active;
+        await updateTask({ variables: { input: { id: task.id, status: newStatus } } });
+    };
+
+    useEffect(() => {
+        if (updateTaskError) {
+            alert('An error occurred, please try again.');
+        }
+    }, [updateTaskError]);
+
    return (
        <li className="task-list-item" key={task.id}>
+           <label className="checkbox">
+               <input
+                   type="checkbox"
+                   onChange={handleStatusChange}
+                   checked={task.status === TaskStatus.Completed}
+                   disabled={updateTaskLoading}
+               />
+               <span className="checkbox-mark">&#10003;</span>
+           </label>
            <Link className="task-list-item-title" href="/update/[id]" as={`/update/${task.id}`}>
                {task.title} ({task.status})
            </Link>
